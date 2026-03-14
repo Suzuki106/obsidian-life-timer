7786bf3ed86e0f6cb8f0759d8fe84d660be73388
suzuki106 <1063046101@qq.com>
2026-03-15 06:35:36 +0800

    feat: 实现倒计时计数功能

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,52 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+const TARGET_DATE = new Date("2035-09-29T00:00:00");
+const UPDATE_INTERVAL_MS = 1000;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class LifeTimerPlugin extends Plugin {
+	private statusBarItemEl: HTMLElement | null = null;
+	private intervalId: number | null = null;
 
-	async onload() {
-		await this.loadSettings();
+	async onload(): Promise<void> {
+		this.statusBarItemEl = this.addStatusBarItem();
+		this.updateCountdown();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		this.intervalId = window.setInterval(() => {
+			this.updateCountdown();
+		}, UPDATE_INTERVAL_MS);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+		this.registerInterval(this.intervalId);
 	}
 
-	onunload() {
+	onunload(): void {
+		if (this.statusBarItemEl) {
+			this.statusBarItemEl.empty();
+		}
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+	private updateCountdown(): void {
+		if (!this.statusBarItemEl) return;
+
+		const now = new Date();
+		const diffMs = TARGET_DATE.getTime() - now.getTime();
+
+		if (diffMs <= 0) {
+			this.statusBarItemEl.setText("Life Timer: 倒计时已结束");
+			return;
+		}
+
+		const totalSeconds = Math.floor(diffMs / 1000);
+		const days = Math.floor(totalSeconds / (24 * 60 * 60));
+		const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+		const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+		const seconds = totalSeconds % 60;
+
+		this.statusBarItemEl.setText(
+			`距离十年结束还有: ${days}天 ${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`,
+		);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	private pad(value: number): string {
+		return value.toString().padStart(2, "0");
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,36 +1,16 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import { App, PluginSettingTab } from "obsidian";
+import LifeTimerPlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
-}
+export class LifeTimerSettingTab extends PluginSettingTab {
+	plugin: LifeTimerPlugin;
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LifeTimerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
